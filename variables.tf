a74b03fdccf5efe714cdc6ec6e195f49553efa83
Kierra Searle <kierra@searle.garden>
2024-04-09 15:35:17 -0400

    feat: added new `endpoint_type` variable to allow you to choose what endpoint to use when communicating with Secrets Manager (#147)

--- variables.tf
+++ variables.tf
@@ -18,5 +18,14 @@ variable "secret_group_description" {
   type        = string
   description = "Description of the Secret Group to be created."
 }
+variable "endpoint_type" {
+  type        = string
+  description = "The service endpoint type to communicate with the provided secrets manager instance. Possible values are `public` or `private`"
+  default     = "public"
+  validation {
+    condition     = contains(["public", "private"], var.endpoint_type)
+    error_message = "The specified endpoint_type is not a valid selection!"
+  }
+}
 
 ##############################################################################
