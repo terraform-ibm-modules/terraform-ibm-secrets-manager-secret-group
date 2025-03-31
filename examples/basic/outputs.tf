602a50d3467a2bf9b5ca480b330e04a42aa031e3
Alex Reiff <alex.reiff@ibm.com>
2025-03-31 10:22:38 -0400

    fix coverage

--- examples/basic/outputs.tf
+++ examples/basic/outputs.tf
@@ -2,9 +2,15 @@
 # Outputs
 ##############################################################################
 
-output "secret_group_id" {
-  description = "ID of the created Secret Group"
-  value       = module.secrets_manager_group_acct.secret_group_id
+output "secret_group_id1" {
+  description = "ID of the first created Secret Group with a matching access group"
+  value       = module.secrets_manager_group_acct1.secret_group_id
+}
+
+
+output "secret_group_id2" {
+  description = "ID of the second created Secret Group"
+  value       = module.secrets_manager_group_acct2.secret_group_id
 }
 
 ##############################################################################
