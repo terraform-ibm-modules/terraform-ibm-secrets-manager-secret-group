92d34a2edfa8d0899b49d2bdacc40c3659823d72
Alex Reiff <alex.reiff@ibm.com>
2025-04-23 13:56:02 -0400

    policy conflict

--- examples/private/main.tf
+++ examples/private/main.tf
@@ -28,16 +28,17 @@ locals {
 }
 
 module "secrets_manager" {
-  source                   = "terraform-ibm-modules/secrets-manager/ibm"
-  version                  = "2.1.1"
-  existing_sm_instance_crn = var.existing_sm_instance_crn
-  resource_group_id        = module.resource_group.resource_group_id
-  region                   = local.sm_region
-  secrets_manager_name     = "${var.prefix}-sm"
-  sm_service_plan          = "trial"
-  allowed_network          = "private-only"
-  endpoint_type            = "private"
-  sm_tags                  = var.resource_tags
+  source                        = "terraform-ibm-modules/secrets-manager/ibm"
+  version                       = "2.1.1"
+  existing_sm_instance_crn      = var.existing_sm_instance_crn
+  resource_group_id             = module.resource_group.resource_group_id
+  region                        = local.sm_region
+  secrets_manager_name          = "${var.prefix}-sm"
+  sm_service_plan               = "trial"
+  allowed_network               = "private-only"
+  endpoint_type                 = "private"
+  sm_tags                       = var.resource_tags
+  skip_iam_authorization_policy = var.skip_iam_authorization_policy
 }
 
 ##################################################################
--- examples/private/variables.tf
+++ examples/private/variables.tf
@@ -39,3 +39,9 @@ variable "existing_sm_instance_region" {
   description = "The region of the existing Secrets Manager instance. Only required if value is passed into var.existing_sm_instance_guid"
   default     = null
 }
+
+variable "skip_iam_authorization_policy" {
+  type        = bool
+  description = "Whether to skip the creation of the IAM authorization policies required to enable the IAM credentials engine. If set to false, policies will be created that grants the Secrets Manager instance 'Operator' access to the IAM identity service, and 'Groups Service Member Manage' access to the IAM groups service."
+  default     = false
+}
