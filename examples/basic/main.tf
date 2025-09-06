c1a4903907c37b72be6aaefd2c1aacbeb5c85aaa
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-09-06 09:13:46 +0100

    fix(deps): update dependencies (#338)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -22,7 +22,7 @@ module "resource_group" {
 module "secrets_manager" {
   count                = var.existing_sm_instance_guid != null ? 0 : 1
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.8.6"
+  version              = "2.8.7"
   secrets_manager_name = "${var.prefix}-sm-instance"
   sm_service_plan      = var.sm_service_plan
   region               = local.sm_region
