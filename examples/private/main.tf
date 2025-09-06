c588fedbbd3f8f1d1ed0f387c5b6b27090444580
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-09-06 06:38:45 +0000

    fix(deps): update dependencies

--- examples/private/main.tf
+++ examples/private/main.tf
@@ -29,7 +29,7 @@ locals {
 
 module "secrets_manager" {
   source                        = "terraform-ibm-modules/secrets-manager/ibm"
-  version                       = "2.8.6"
+  version                       = "2.8.7"
   existing_sm_instance_crn      = var.existing_sm_instance_crn
   resource_group_id             = module.resource_group.resource_group_id
   region                        = local.sm_region
