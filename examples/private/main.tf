ff72b722438de3090bcbb1a6f904c5b4e7eba734
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-02 23:52:41 +0000

    chore(deps): update terraform terraform-ibm-modules/secrets-manager/ibm to v2.12.14

--- examples/private/main.tf
+++ examples/private/main.tf
@@ -29,7 +29,7 @@ locals {
 
 module "secrets_manager" {
   source                        = "terraform-ibm-modules/secrets-manager/ibm"
-  version                       = "2.12.13"
+  version                       = "2.12.14"
   existing_sm_instance_crn      = var.existing_sm_instance_crn
   resource_group_id             = module.resource_group.resource_group_id
   region                        = local.sm_region
