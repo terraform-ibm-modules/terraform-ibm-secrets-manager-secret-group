c527cf2b6ddc0c67281a436561c681ccd9d8c968
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2026-01-27 14:30:46 +0000

    fix: only return secret_group_name after group is created

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,8 @@ output "secret_group_id" {
 output "secret_group_name" {
   description = "Name of the created Secret Group"
   value       = ibm_sm_secret_group.secret_group.name
+  # as the name is not computed by the provider, using explicit depends_on to only return the name AFTER the group has been created.
+  depends_on  = [ibm_sm_secret_group.secret_group]
 }
 
 output "access_group_id" {
