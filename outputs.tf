269ee4265b7bd1e0f5e14406eb8b61cbfb3f9de5
Rajat Agrawal <108140212+rajatagarwal-ibm@users.noreply.github.com>
2026-01-27 14:18:41 +0000

    feat: added new module output `secret_group_name` (#392)
    Co-authored-by: Rajat Agrawal <rajat.agrawal@ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,11 @@ output "secret_group_id" {
   value       = ibm_sm_secret_group.secret_group.secret_group_id
 }
 
+output "secret_group_name" {
+  description = "Name of the created Secret Group"
+  value       = ibm_sm_secret_group.secret_group.name
+}
+
 output "access_group_id" {
   description = "ID of the created Access Group"
   value       = local.access_group_id
